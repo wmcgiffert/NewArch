9c3d7c5c840d63809c4e74ddee8f195b00757c4d
William McGiffert <wmcgiffert@crimson.ua.edu>
2022-02-22 15:54:37 -0600

    ConnectedData project to the Domain level

---
+++ NewArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using NewArch.Domain.Interfaces;
+using NewArch.Domain.Models;
+using NewArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
