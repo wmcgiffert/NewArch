705167fdf50f865c7c0be59cdc2cc01b90ef387f
William McGiffert <William.McGiffert@alliancebernstein.com>
2022-02-22 15:13:22 -0600

    Added CourseVM Added CourseInterface

---
+++ NewArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using NewArch.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewArch.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseVM> GetCourses();
+    }
+}
---
+++ NewArch.Application/ViewModels/CourseVM.cs
@@ -0,0 +1,14 @@
+﻿using NewArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewArch.Application.ViewModels
+{
+    public class CourseVM
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
