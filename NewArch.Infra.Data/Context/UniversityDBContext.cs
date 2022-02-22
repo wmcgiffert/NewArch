60f1f68a881564e4c902166a20b21e3a2db3562c
William McGiffert <William.McGiffert@alliancebernstein.com>
2022-02-22 14:13:43 -0600

    Class renamed add dependenceies added constructors and properties

---
+++ NewArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
