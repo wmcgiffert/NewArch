06ab776f48b77d11f53faefd0498721c44036ddf
William McGiffert <William.McGiffert@alliancebernstein.com>
2022-02-22 15:22:23 -0600

    Added Domain Course Repository Interface

---
+++ NewArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using NewArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewArch.Domain.Interfaces
+{
+    internal interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
