8897c23d7b4ca6bdb5165854824724818630de40
William McGiffert <wmcgiffert@crimson.ua.edu>
2022-03-01 13:22:29 -0600

    Added View Connected VM to View

---
+++ NewArch/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewArch.Application.Interfaces;
+using NewArch.Application.ViewModels;
+
+namespace NewArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseVM model = _courseService.GetCourses()
+            return View(model);
+        }
+    }
+}
