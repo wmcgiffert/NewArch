0affa6d319f648c364843f40744c1e260da8e151
William McGiffert <wmcgiffert@crimson.ua.edu>
2022-04-13 14:52:00 -0500

    added infra.bus class libaray and external API

---
+++ NewArch.Infra.Bus/InMemoryBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewArch.Infra.Bus
+{
+    public class InMemoryBus
+    {
+    }
+}
